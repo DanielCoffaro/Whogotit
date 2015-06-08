1ba041d888e45b2384f0c5ad1b8f5df42f148265
Danimal91 <Danimal91@gmail.com>
2015-06-08 16:34:09 -0400

    JS Changes

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,21 @@
         });
     };
 
+    var list = document.getElementsById('stuff')
+
+    var render = function(){
+        borrowed.forEach(function(item){
+            var listItem = document.createElement('li')
+            var text = document.createTextNode(item.who + ' has my ' + item.what + '(' + item.when ')');
+
+            listItem.appendChild(text);
+            list.appendChild(text);
+        });
+    }:
+
     addItem('Dan,The Good Parts', Date.now());
 
     console.log(borrowed);
+
+    render();
 }());
