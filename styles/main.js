ad190ad0a4c311ea57b04e521b7de0f8e5764208
Danimal91 <Danimal91@gmail.com>
2015-06-08 16:10:49 -0400

    st

--- styles/main.js
+++ styles/main.js
@@ -1,4 +1,17 @@
+'use strict';
+
 (function(){
+    var borrowed = [];
 
+    var addItem() = function(who,what,when){
+        borrowed.push({
+        who: who,
+        what: what,
+        when: when
+        });
+    };
 
+    addItem('Dan,The Good Parts', new Date().now());
+
+    console.log(borrowed);
 })();
